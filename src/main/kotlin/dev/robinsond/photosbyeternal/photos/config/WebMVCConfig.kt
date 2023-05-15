7905ec442bf0fed281aa10a603fb485a7cfa15cd
EternalDev1 <robinsond.dev@gmail.com>
2023-05-15 07:17:16 -0700

    update config

--- src/main/kotlin/dev/robinsond/photosbyeternal/photos/config/WebMVCConfig.kt
+++ src/main/kotlin/dev/robinsond/photosbyeternal/photos/config/WebMVCConfig.kt
@@ -1,14 +1,19 @@
 package dev.robinsond.photosbyeternal.photos.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMVCConfig: WebMvcConfigurer {
+
+    @Value("\${allowed.origins}")
+    private val allowedOrigins: String? = null
+
     override fun addCorsMappings(corsRegistry: CorsRegistry) {
         corsRegistry.addMapping("/**")
-            .allowedOrigins("http://localhost:8081")
+            .allowedOrigins(allowedOrigins.split(","))
             .allowedMethods("*")
             .allowedHeaders("*")
     }
